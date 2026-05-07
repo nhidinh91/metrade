b9aa7a94cc6b19a18519ba005b1da7e9ba2a19e4
Nhi Dinh <142903360+nhidinh91@users.noreply.github.com>
2026-05-07 11:47:33 +0300

    Update database.js

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,14 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        if (process.env.NODE_ENV === 'development') {
-            console.log('Connected to MongoDB');
-        }
-    } catch (error) {
-        console.log('Error connecting to MongoDB:', error);
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    throw error;
+  }
+};
 
 export default connectDB;
